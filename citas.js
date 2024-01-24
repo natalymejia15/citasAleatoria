3956319c1d1a011342244750ed390b0a4361f10e
Nataly <practicante_planea3@corantioquia.gov.co>
2024-01-24 13:41:50 -0500

    first commit

---
+++ citas.js
@@ -0,0 +1,50 @@
+let citas = [
+  {
+    autor: "Walt Disney",
+    texto: "La forma de empezar es dejar de hablar y empezar a hacer.",
+  },
+  {
+    autor: "Nelson Mandela",
+    texto:
+      "El mayor glory en vivir no radica en nunca caer, sino en levantarnos cada vez que caemos.",
+  },
+  {
+    autor: "Steve Jobs",
+    texto:
+      "Tu trabajo va a llenar gran parte de tu vida, y la única manera de estar realmente satisfecho es hacer lo que crees que es un gran trabajo.",
+  },
+  {
+    autor: "Oprah Winfrey",
+    texto:
+      "Haz lo que tienes que hacer, hasta que puedas hacer lo que quieres hacer.",
+  },
+  {
+    autor: "Albert Einstein",
+    texto:
+      "La vida es como andar en bicicleta, para mantener el equilibrio, debes seguir adelante.",
+  },
+  {
+    autor: "Confucio",
+    texto:
+      "Nuestra mayor gloria no radica en no haber caído nunca, sino en levantarnos cada vez que caemos.",
+  },
+  {
+    autor: "Helen Keller",
+    texto:
+      "La seguridad es en gran medida una superstición. La vida es una aventura atrevida o no es nada en absoluto.",
+  },
+  {
+    autor: "Vince Lombardi",
+    texto:
+      "La perfección no es alcanzable, pero si perseguimos la perfección, podemos alcanzar la excelencia.",
+  },
+  {
+    autor: "Maya Angelou",
+    texto:
+      "Nada puede hacer que regrese el dolor del pasado, excepto tú mismo. No hay error, ni mal, ni tragedia. Solo amor.",
+  },
+  {
+    autor: "Thomas Edison",
+    texto: "No he fracasado. He encontrado 10,000 maneras que no funcionarán.",
+  },
+];
